bfd9d8a201137c1c2d59c20fe6c3d0972167c31c
marcoschultznunes <marco_sn@hotmail.com>
2022-06-21 17:01:58 -0300

    Cart icon, FA, responsive navbar CSS

--- src/components/navbar/CartButton.tsx
+++ src/components/navbar/CartButton.tsx
@@ -1,10 +1,12 @@
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
 import "./CartButton.css";
 
 const CartButton:FC = (props) => {
 
     return <button className="cart-button">
-        {/* icon */}
+        <FontAwesomeIcon icon={faCartShopping} className="cart-button-icon"/>
         <span>Your Cart</span>
         <span className="cart-button-quantity">4</span>
     </button>;
