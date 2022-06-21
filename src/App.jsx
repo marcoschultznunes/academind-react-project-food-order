7989a9e85457ce5047486e1297c3c5cc03692cb6
marcoschultznunes <marco_sn@hotmail.com>
2022-06-21 16:05:09 -0300

    Typescript setup

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-const App = () => {
-  
-  return <div>
-    <h1>Welcome to the Pizzeria</h1>
-  </div>; 
-};
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <h3>Hello There!</h3>;
+};
+
+export default App;
