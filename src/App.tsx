4bbed4fef5c4c79d34f76646b04fe7fd849e2e29
marcoschultznunes <marco_sn@hotmail.com>
2022-06-22 13:05:10 -0300

    Summary component w/ responsive CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import Summary from './components/summary/Summary';
 
 function App() {
   return <React.Fragment> 
     <Navbar />
+    <Summary />
   </React.Fragment>;
 };
 
---
+++ src/components/summary/Summary.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import "./Summary.css";
+
+const Summary: FC = (props) => {
+
+  return <div className="summary-container">
+    <h2>Delicious Food, Delivered To You</h2>
+    <p>
+      Choose your favorite meal from our broad selection of available meals
+      and enjoy a delicious lunch or dinner at home.
+    </p>
+    <p>
+      All our meals are cooked with high-quality ingredients, just-in-time and
+      of course by experienced chefs!
+    </p>
+  </div>;
+};
+
+export default Summary;
