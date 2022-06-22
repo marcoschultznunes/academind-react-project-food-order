103af8c30945b6af48f953494864c8d7716b68aa
marcoschultznunes <marco_sn@hotmail.com>
2022-06-22 14:11:52 -0300

    Meals list items controls styled

--- src/components/meals/MealsListItem.tsx
+++ src/components/meals/MealsListItem.tsx
@@ -1,31 +1,30 @@
-import {FC} from "react";
+import { FC } from "react";
 import "./MealsListItem.css";
 
 type MealsListItemProps = {
-  title: string
-  description: string
-  price: number
+    title: string
+    description: string
+    price: number
 };
 
-const MealsListItem:FC<MealsListItemProps> = (props) => {
-  const {title, description, price} = props;
+const MealsListItem: FC<MealsListItemProps> = (props) => {
+    const { title, description, price } = props;
 
-  return <li className="meals-list-item-container">
-    <div className="meals-list-item-details">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <span>${price}</span>
-    </div>
-    <div className="meals-list-item-controls">
-      <div className="amount-input-container">
-        <label htmlFor="amount">Amount</label>
-        <input type="text" name="amount" />
-      </div>
-      <div>
-        <button>+ Add</button>
-      </div>
-    </div>
-  </li>;
+    return <li className="meals-list-item-container">
+        <div className="meals-list-item-details">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <span>${price}</span>
+        </div>
+        <div className="meals-list-item-controls">
+            <div>
+                <p>3 in Cart</p>
+            </div>
+            <div>
+                <button>+ Add</button>
+            </div>
+        </div>
+    </li>;
 };
 
 export default MealsListItem;
